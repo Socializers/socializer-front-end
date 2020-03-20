7dd59aa80492f215e9c1405b4606e8967dacc924
ibraim-Ajarmeh <ajarmeh1997@gmail.com>
2020-03-20 15:28:42 +0200

    the skeleton of the app

---
+++ src/components/app.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+import Header from './header';
+/// main
+import Footer from './footer';
+
+
+export default function App() {
+  return(
+    <>
+      <Header />
+      {/* <Main /> */}
+      <Footer />
+    </>
+  );
+}
